21a5c2118900d034207b0c92323b3c81e3bd3465
Kamalesh P <158757738+kamalesh574@users.noreply.github.com>
2026-02-22 21:31:40 +0530

    Update SecurityConfig.java

--- ContactPro (Contact Management System)/contact-manager/src/main/java/com/contactmanager/config/SecurityConfig.java
+++ ContactPro (Contact Management System)/contact-manager/src/main/java/com/contactmanager/config/SecurityConfig.java
@@ -48,7 +48,10 @@ public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSou
     org.springframework.web.cors.CorsConfiguration configuration =
             new org.springframework.web.cors.CorsConfiguration();
 
-    configuration.setAllowedOriginPatterns(java.util.List.of("*"));
+    configuration.setAllowedOrigins(java.util.List.of(
+        "http://localhost:5173",
+        "https://contact-manager-wheat-seven.vercel.app"
+));
     configuration.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(java.util.List.of("*"));
     configuration.setAllowCredentials(true);
