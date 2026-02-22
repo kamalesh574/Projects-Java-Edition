de844ecc5a9c4dbffaab7e47e19d180e2bbef878
Kamalesh P <158757738+kamalesh574@users.noreply.github.com>
2026-02-22 13:41:54 +0530

    Update axios.ts

--- ContactPro (Contact Management System)/contact-manager-frontend/src/api/axios.ts
+++ ContactPro (Contact Management System)/contact-manager-frontend/src/api/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8081",
+  const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});,
 });
 
 // ✅ Set Basic Auth header
